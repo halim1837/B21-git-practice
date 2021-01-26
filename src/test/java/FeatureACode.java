2032761451ab86e580e967477da9be41ac5918ec
halim1837 <halim.bakhazi@gmail.com>
2021-01-26 14:24:45 -0500

    more work added to feature A

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -2,6 +2,6 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A started");
-
+        System.out.println("More git review");
     }
 }
