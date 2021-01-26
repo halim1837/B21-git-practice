61efb8b7317367ee29e067558601529d7d0f52af
halim1837 <halim.bakhazi@gmail.com>
2021-01-26 15:24:45 -0500

    movie 1 branch work is done

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("Testing is my passion");
+        System.out.println("ready to go back");
     }
 }
